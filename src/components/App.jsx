ce3aba603aa25570afaabdb98ec9d8726641c802
katsukiwataru <a17dc050@dhw.ac.jp>
2019-06-18 22:03:03 +0900

    delete state text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import List from './List';
+
 
 export default class App extends Component {
-  constructor(props) {
-    console.log(props)
-    super(props);
-    this.state = {
-      text: "title",
-    }
-  }
   render() {
-    if (this.state.text) {
-      return (
-        <div className="siimple-box siimple--bg-dark">
-          <h1 className="siimple-box-title siimple--color-white">{this.state.text}</h1>
-          <Form/>
-        </div>
-      )
-    } else {
-      return (
-        <div className="siimple-box">
-          <h1 className="siimple-box-title siimple--color-white">erro</h1>
-          <Form />
-        </div>
-      )
-    }
+    return (
+      <div className="siimple-box siimple--bg-dark">
+        <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
+        <Form />
+        <div className="siimple-rule"></div>
+        <List />
+      </div>
+    );
   }
 }
