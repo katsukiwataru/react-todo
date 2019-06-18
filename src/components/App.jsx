0529b7f29a50ade2c34b1118e5ae35464befc566
katsukiwataru <a17dc050@dhw.ac.jp>
2019-06-18 22:38:16 +0900

    add constructor and handleAdd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import Form from './Form';
 import List from './List';
 
-
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("props");
+      this.state = {
+        todo: []
+      };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+  handleAdd(e) {
+    console.log(e.target.title.value);
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="siimple-box siimple--bg-dark">
         <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
-        <Form />
+        <Form handleAdd={this.handleAdd} />
         <div className="siimple-rule"></div>
         <List />
       </div>
