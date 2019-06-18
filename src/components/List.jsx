bfcf4ab9679ec56299f68934762597861bb44aac
katsukiwataru <a17dc050@dhw.ac.jp>
2019-06-18 20:47:20 +0900

    fix

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,12 +8,12 @@ let btn = {
   cursor: 'pointer'
 };
 
-const List = (props) => {
+const List = (props) => (
   <ul className="siimple-list">
     <li className="siimple-list-item siimple--bg-white" style={style}>Item 1 <span className="siimple-tag siimple-tag--error siimple-hover" style={btn}>Delete</span></li>
     <li className="siimple-list-item siimple--bg-white" style={style}>Item 2 <span className="siimple-tag siimple-tag--error siimple-hover" style={btn}>Delete</span></li>
     <li className="siimple-list-item siimple--bg-white" style={style}>Item 3 <span className="siimple-tag siimple-tag--error siimple-hover" style={btn}>Delete</span></li>
   </ul>
-}
+);
 
 export default List;
