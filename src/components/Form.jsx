9f72f523807e058a5ec85cdd63e5c208c38c312d
katsukiwataru <a17dc050@dhw.ac.jp>
2019-06-19 21:28:20 +0900

    delete Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Form = (props) => (
   <form className="siimple-form" onSubmit={props.handleAdd}>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 let style = { maxWidth: '700px' };
 
